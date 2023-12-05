d13fd113c0498044e80f55cd9b1d79cc29cece83
isidi <slavaivankov236@gmail.com>
2023-12-05 15:33:06 +0300

    init

---
+++ src/test/java/com/is1di/quoteapi/QuoteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.is1di.quoteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
